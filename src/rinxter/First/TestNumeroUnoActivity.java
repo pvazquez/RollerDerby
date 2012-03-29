342a7c878dd24cfee480bd4ae10f64078643ea7a
Steve <stephen@ubuntu.ubuntu-domain>
2012-03-29 12:50:21 -0400

    HERE!

---
+++ src/rinxter/First/TestNumeroUnoActivity.java
@@ -0,0 +1,13 @@
+package rinxter.First;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestNumeroUnoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
