05801624e4d3088cce9f53d90172b861a01f5220
Steve <stephen@ubuntu.ubuntu-domain>
2012-04-03 12:19:30 -0400

    ARRRG

--- src/rinxter/First/TestNumeroUnoActivity.java
+++ src/rinxter/First/TestNumeroUnoActivity.java
@@ -1,13 +1,25 @@
 package rinxter.First;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestNumeroUnoActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        final Intent i =  new Intent(this, SecondScreenActivity.class);
+        Button b = (Button) findViewById(rinxter.First.R.id.btn1);
+        b.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				startActivity(i);
+			}
+        });
+        setContentView(rinxter.First.R.layout.main);
     }
+	
 }
